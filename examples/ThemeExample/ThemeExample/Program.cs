392c4e443682e2a7486462753573b79f954b1427
Eray594 <eray.kaan.private07@gmail.com>
2025-03-01 22:44:06 +0100

    Stable Version 1.0

--- examples/ThemeExample/ThemeExample/Program.cs
+++ examples/ThemeExample/ThemeExample/Program.cs
@@ -1,11 +1,22 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MudForge.Demo;
+using MudBlazor;
+using ThemeExample;
+using MudBlazor.Services;
+using MudForge.Theming;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
+
+builder.Services.AddMudThemeServices(new MudThemeServiceConfiguration
+{
+    IsDarkMode = Resources.IsDarkMode,
+    LocalStorageKey = Resources.LocalStorageKey,
+    Theme = Resources.Theme,
+});
 
 await builder.Build().RunAsync();
